66f74a332942db644eae6b47672116ab9acab434
leaderman77 <sardorasatillaev@gmail.com>
2023-03-26 15:37:25 +0500

    visuilized dataset annotations

--- create_dataset.py
+++ create_dataset.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from utils import split_dataset
 
 
 def extract_info_from_csv(csv_file):
@@ -15,25 +16,25 @@ def convert_to_yolov8(info_dict):
     print_buffer = []
 
     # Get the values of the dictionary as a list
-    values_xmin_list = list(info_dict["bbox_x"].values())       # xmin
-    values_ymin_list = list(info_dict["bbox_y"].values())       # ymin
-    values_xmax_list = list(info_dict["bbox_width"].values())   # xmax
-    values_ymax_list = list(info_dict["bbox_height"].values())  # ymax
-    values_iw_list = list(info_dict["image_width"].values())
-    values_ih_list = list(info_dict["image_height"].values())
+    xmin_list = list(info_dict["bbox_x"].values())       # xmin
+    ymin_list = list(info_dict["bbox_y"].values())       # ymin
+    xmax_list = list(info_dict["bbox_width"].values())   # xmax
+    ymax_list = list(info_dict["bbox_height"].values())  # ymax
+    iw_list = list(info_dict["image_width"].values())
+    ih_list = list(info_dict["image_height"].values())
 
     # For each bounding box
-    for i in range(len(values_xmin_list)):
-        b_center_x = (values_xmin_list[i] + values_xmax_list[i]) / 2
-        b_center_y = (values_ymin_list[i] + values_ymax_list[i]) / 2
-        b_width = (values_xmax_list[i] - values_xmin_list[i])
-        b_height = (values_ymax_list[i] - values_ymin_list[i])
+    for i in range(len(xmin_list)):
+        b_center_x = (xmin_list[i] + xmax_list[i]) / 2
+        b_center_y = (ymin_list[i] + ymax_list[i]) / 2
+        b_width = (xmax_list[i] - xmin_list[i])
+        b_height = (ymax_list[i] - ymin_list[i])
 
         # Normalise the co-ordinates by the dimensions of the image
-        b_center_x /= values_iw_list[i]
-        b_center_y /= values_ih_list[i]
-        b_width /= values_iw_list[i]
-        b_height /= values_ih_list[i]
+        b_center_x /= iw_list[i]
+        b_center_y /= ih_list[i]
+        b_width /= iw_list[i]
+        b_height /= ih_list[i]
         b_height = abs(b_height)
 
         # Write the bbox details to the file
@@ -57,3 +58,5 @@ info_dict = extract_info_from_csv(csv_file)
 annot_list = convert_to_yolov8(info_dict)
 
 save_annotation_to_txt(info_dict, annot_list)
+
+split_dataset()
--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 import os
 import shutil
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 from sklearn.model_selection import train_test_split
+
+class_id = 0
 
 
 # Utility function to move images
@@ -8,9 +14,12 @@ def move_files_to_folder(list_of_files, destination_folder):
     for f in list_of_files:
         try:
             shutil.move(f, destination_folder)
-        except:
-            print(f)
-            assert False
+        # except:
+        #     print(f)
+        #     assert False
+        except Exception as e:
+            print(f"Error moving file {f}: {e}")
+            raise
 
 
 def split_dataset():
@@ -28,12 +37,52 @@ def split_dataset():
         val_images, val_annotations, test_size=0.5, random_state=1)
 
     # Move the splits into their folders
-    move_files_to_folder(train_images, 'images/train')
-    move_files_to_folder(val_images, 'images/val/')
-    move_files_to_folder(test_images, 'images/test/')
-    move_files_to_folder(train_annotations, 'annotations/train/')
-    move_files_to_folder(val_annotations, 'annotations/val/')
-    move_files_to_folder(test_annotations, 'annotations/test/')
+    move_files_to_folder(train_images, 'dataset/train/images')
+    move_files_to_folder(val_images, 'dataset/val/images')
+    move_files_to_folder(test_images, 'dataset/test/images')
+    move_files_to_folder(train_annotations, 'dataset/train/labels')
+    move_files_to_folder(val_annotations, 'dataset/val/labels')
+    move_files_to_folder(test_annotations, 'dataset/test/labels')
 
 
-split_dataset()
+def plot_bounding_box_cv2(image, annotation_list):
+    annotations = np.array(annotation_list)
+    h, w, _ = image.shape
+    for ann in annotations:
+        obj_cls, center_x, center_y, width, height = ann
+        x0 = int((center_x - width / 2) * w)
+        y0 = int((center_y - height / 2) * h)
+        x1 = int((center_x + width / 2) * w)
+        y1 = int((center_y + height / 2) * h)
+
+        print(ann)
+        print(x0, y0, x1, y1)
+        cv.rectangle(image, (x0, y0), (x1, y1), (0, 255, 0), 2)
+        cv.putText(image, str(obj_cls), (x0, y0 - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+    return image
+
+
+def plot_bounding_box(image, annotation_list):
+    annotations = np.array(annotation_list)
+    w, h = image.size
+
+    plotted_image = ImageDraw.Draw(image)
+
+    transformed_annotations = np.copy(annotations)
+    transformed_annotations[:, [1, 3]] = annotations[:, [1, 3]] * w
+    transformed_annotations[:, [2, 4]] = annotations[:, [2, 4]] * h
+
+    transformed_annotations[:, 1] = transformed_annotations[:, 1] - (transformed_annotations[:, 3] / 2)
+    transformed_annotations[:, 2] = transformed_annotations[:, 2] - (transformed_annotations[:, 4] / 2)
+    transformed_annotations[:, 3] = transformed_annotations[:, 1] + transformed_annotations[:, 3]
+    transformed_annotations[:, 4] = transformed_annotations[:, 2] + transformed_annotations[:, 4]
+
+    for ann in transformed_annotations:
+        obj_cls, x0, y0, x1, y1 = ann
+        plotted_image.rectangle(((x0, y0), (x1, y1)))
+        print(obj_cls, x0, y0, x1, y1)
+        plotted_image.text((x0, y0 - 10), [class_id])
+
+    plt.imshow(np.array(image))
+    plt.show()
