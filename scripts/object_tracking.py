7391a88f4171598aa441153bedc9248676b1b588
leaderman77 <sardorasatillaev@gmail.com>
2023-04-13 00:21:39 +0500

    changed function names

--- scripts/object_tracking.py
+++ scripts/object_tracking.py
@@ -3,45 +3,40 @@ import cv2 as cv
 import supervision as sv
 from ultralytics import YOLO
 
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+VIDEO_FILE = "data/video/vid_39_1284-2_3881.mp4"
+MODEL_PATH = "models/best.pt"
+DISPLAY_SIZE = (800, 600)
+
 # necessary for counting
 # LINE_START = sv.Point(320, 0)
 # LINE_END = sv.Point(320, 480)
 
 
-def load_model():
-    project_dir = os.path.dirname(os.path.dirname(__file__))
-    model_path = os.path.join(project_dir, "models", "best.pt")
-    source_path = os.path.join(project_dir, "data", "video", "vid_39_1284-2_3881.mp4")
+def tracking_objects(model_path, video_path, show=True, conf=0.2, iou=0.1, stream=True):
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
     # model = YOLO("yolov8n.pt")
-    model = YOLO(model_path)
-    results = model.track(
-        source=source_path,
-        show=True,
-        conf=0.1,
-        iou=0.1,
-        stream=True,
+    yolo_model = YOLO(model_path)
+
+    results = yolo_model.track(
+        source=video_path,
+        show=show,
+        conf=conf,
+        iou=iou,
+        stream=stream,
         agnostic_nms=True)
 
-    return results, model
+    return results, yolo_model
 
 
-def tracking():
+def display_tracking(tracking_results, yolo_model):
 
-    display_size = (800, 600)
     # line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
     # line_annotator = sv.LineZoneAnnotator(thickness=2, text_thickness=1, text_scale=0.5)
-    box_annotator = sv.BoxAnnotator(
-        thickness=2,
-        text_thickness=1,
-        text_scale=0.5
-    )
+    box_annotator = sv.BoxAnnotator(thickness=2, text_thickness=1, text_scale=0.5)
 
-    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-    results, model = load_model()
-
-    # for result in model.track(output=0, show=True, stream=True, agnostic_nms=True):
-    for result in results:
+    for result in tracking_results:
         frame = result.orig_img
         detections = sv.Detections.from_yolov8(result)
 
@@ -51,7 +46,7 @@ def tracking():
         # detections = detections[(detections.class_id != 60) & (detections.class_id != 0)]
 
         labels = [
-            f"{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
+            f"{tracker_id} {yolo_model.model.names[class_id]} {confidence:0.2f}"
             for _, confidence, class_id, tracker_id
             in detections
         ]
@@ -64,14 +59,23 @@ def tracking():
 
         # line_counter.trigger(detections=detections)
         # line_annotator.annotate(frame=frame, line_counter=line_counter)
+
         # resize the frame to fit the display window
-        frame = cv.resize(frame, display_size)
+        frame = cv.resize(frame, DISPLAY_SIZE)
         cv.imshow("yolov8", frame)
 
-        # if (cv2.waitKey(30) == 27):
         if cv.waitKey(1) == ord("q"):
             break
 
 
+def main():
+
+    # model and video path
+    model_path = os.path.join(PROJECT_DIR, MODEL_PATH)
+    video_path = os.path.join(PROJECT_DIR, "data", "video", "vid_39_1284-2_3881.mp4")
+    tracking_results, yolo_model = tracking_objects(model_path, video_path)
+    display_tracking(tracking_results, yolo_model)
+
+
 if __name__ == "__main__":
-    tracking()
+    main()
