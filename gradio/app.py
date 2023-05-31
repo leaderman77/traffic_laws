cca64e416f50b014de269d3de05e0d062d9a5cc8
leaderman77 <sardorasatillaev@gmail.com>
2023-05-31 15:18:23 +0500

    minor changes

--- gradio/app.py
+++ gradio/app.py
@@ -5,8 +5,8 @@ import gradio as gr
 import pandas as pd
 from ultralytics import YOLO
 
-PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
-model = YOLO(os.path.join(PROJECT_DIR, '../models', 'detection', 'best.pt'))
+PROJECT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+model = YOLO(os.path.join(PROJECT_DIR, 'models', 'detection', 'best.pt'))
 
 class_names = ['qizil', 'yashil', 'chiziq', 'keng_mashina_chiziqda']
 colors = np.random.default_rng(3).uniform(0, 255, size=(len(class_names), 3))
@@ -14,17 +14,17 @@ colors = np.random.default_rng(3).uniform(0, 255, size=(len(class_names), 3))
 
 def draw_detections(image, boxes, scores, cls_id, mask_alpha=0.3):
     """
-    Kiritilgan rasmga bbox, aniqlik koifisent va kass idisini chizish funksiyasi
+    Kiritilgan rasmga bbox aniqlik darajasi va klass IDisini chizish funksiyasi
 
     Parameterlar:
-        - rasm (numpy.ndarray): detectionni chizish uchun berilgan rasm.
-        - boxes (List[numpy.ndarray]): [x1, y1, x2, y2] formatidagi bbox koordinatalari ro'yxati.
-        - scores (List[float]): Har bir detection uchun koifisent ballar.
-        - cls_id (int): Objekt idisi.
-        - mask_alpha (float): default 0.3ga teng
+    rasm (numpy.ndarray): detectionni chizish uchun berilgan rasm.
+    boxes (List[numpy.ndarray]): [x1, y1, x2, y2] formatidagi bbox koordinatalari ro'yxati.
+    scores (List[float]): Har bir detection uchun koifisent ballar.
+    cls_id (int): Objekt idisi.
+    mask_alpha (float): default 0.3ga teng
 
     Returns:
-        - numpy.ndarray: bbox detection chizilgan rasm.
+    numpy.ndarray: bbox detection chizilgan rasm.
     """
     mask_img = image.copy()
     det_img = image.copy()
@@ -55,12 +55,13 @@ def detect_objects_on_video(video_path):
     framelarni saqlash funksiyasi.
     Funksiya 2ta fayl yaratadi: Birinchisi, aniqlangan muammoli framelarni va
     ularni vaqtini csv shaklida faylga saqlidi. Ikkinchisi, aniqlangan muammoli
-    framelar asosida vidyo fayl
-    Parameters:
-        - video_path (str): vidyo fayl joylashgan joy.
+    framelar asosida video fayl
+
+    Parameterlar:
+    video_path (str): video fayl joylashgan joy.
 
     Returns:
-        - str: string korinishidagi vidyo resultat.
+    str: string korinishidagi video resultat.
     """
     cap = cv2.VideoCapture(video_path)
     fourcc = cv2.VideoWriter_fourcc(*'h264')
@@ -71,7 +72,7 @@ def detect_objects_on_video(video_path):
     frame_number = 0
 
     while True:
-        # Vidyodan framelarni o'qish
+        # Videodan framelarni o'qish
         ret, frame = cap.read()
 
         if not ret:
@@ -105,7 +106,7 @@ def detect_objects_on_video(video_path):
         # Annotation bor framelarni raqamini chop etish
         if has_annotation:
             annotated_frames.append(frame_number)
-            # muammoli framelarni vidyodagi vaqtini aniqlash va chop etish
+            # muammoli framelarni videodagi vaqtini aniqlash va chop etish
             annotated_timestamps.append(cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0)
             print("Frame", frame_number, "has the annotation")
 
