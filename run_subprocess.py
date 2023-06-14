9ae6bc1b5960d89bc9d371e7b8802cbaca0d7b84
Samariddin Kushmuratov <bekmezonali@gmail.com>
2023-06-14 17:42:10 +0500

    enterni automaticlly bosish qoshildi

--- run_subprocess.py
+++ run_subprocess.py
@@ -6,4 +6,10 @@ os.chdir("binaries/linux/x86_64")
 
 # construct and run the command
 command = "PYTHONPATH=$PYTHONPATH:.:../../../python LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH python ../../../samples/python/recognizer/recognizer.py --image ../../../assets/images/lic_us_1280x720.jpg --assets ../../../assets"
-subprocess.run(command, shell=True)
+process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
+
+# send newline character to standard input
+process.communicate(input='\n'.encode())
+
+# wait for the subprocess to exit
+process.wait()
