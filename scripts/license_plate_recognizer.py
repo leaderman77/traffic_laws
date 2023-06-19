9d073bd1e7b7ee2ffa88f540c2393420e6a04ab0
leaderman77 <sardorasatillaev@gmail.com>
2023-06-19 17:01:06 +0500

    image_path is moved to out of class

--- scripts/license_plate_recognizer.py
+++ scripts/license_plate_recognizer.py
@@ -14,7 +14,11 @@ import ultimateAlprSdk
 
 
 class LicensePlateRecognizer:
-    def __init__(self, json_config, image_types_mapping):
+    def __init__(self,
+                 image_types_mapping,
+                 is_save=False,
+                 is_result_show=False,
+                 save_path=""):
         """
         LicensePlateRecognizer classini instansiyasini ishga tushirish.
 
@@ -24,14 +28,10 @@ class LicensePlateRecognizer:
         image_types_mapping : dict
             Rasm rejimlarini ularning mos keladigan turlariga solishtirish.
         """
-        self.image_folder = os.path.join(PROJECT_DIR, 'assets', 'images')
-        self.charset = "latin"
-        self.assets_folder = os.path.join(PROJECT_DIR, 'assets')
-        self.image_save_path = os.path.join(PROJECT_DIR, 'assets', 'result_imgs')
-        self.is_save = True
-        self.is_result_show = False
-        self.json_config = json_config
         self.image_types_mapping = image_types_mapping
+        self.image_save_path = save_path
+        self.is_save = is_save
+        self.is_result_show = is_result_show
         self.result_dict = {}
 
     def load_cv2_image(self, path):
@@ -54,7 +54,7 @@ class LicensePlateRecognizer:
         if image is None:
             raise ValueError("Failed to load image: %s" % path)
 
-        # tasvirning yo'nalishini tekshirish.
+        # rasmning yo'nalishini tekshirish.
         orientation = 1
         try:
             pil_image = Image.open(path)
@@ -68,7 +68,7 @@ class LicensePlateRecognizer:
             print("An exception occurred: {}".format(e))
             traceback.print_exc()
 
-        # Agar tasvir 1 dan katta orientatsiya qiymatiga ega bo'lsa,
+        # Agar rasm 1 dan katta orientatsiya qiymatiga ega bo'lsa,
         # u mos ravishda aylantiriladi.
         if orientation > 1:
             rotate_code = cv2.ROTATE_90_CLOCKWISE if orientation == 6 else cv2.ROTATE_90_COUNTERCLOCKWISE
@@ -178,7 +178,7 @@ class LicensePlateRecognizer:
 
             self.result_dict = result_dict
 
-    def process_images(self):
+    def process_images(self, image_path):
         """
         Ultimate ALPR SDK dan foydalangan holda belgilangan rasm papkasidan
         rasmlarni qayta ishlash.
@@ -187,40 +187,29 @@ class LicensePlateRecognizer:
         OSError
             Agar rasm papkasida fayl mavjud bo'lmasa.
         """
-        self.json_config["assets_folder"] = self.assets_folder
-        self.json_config["charset"] = self.charset
+        image, image_type = self.load_cv2_image(image_path)
+        height, width, channels = image.shape
+        results = ultimateAlprSdk.UltAlprSdkEngine_process(
+            image_type,
+            image.tobytes(),
+            width,
+            height,
+            0,
+            1
+        )
 
-        self.check_result("Init", ultimateAlprSdk.UltAlprSdkEngine_init(json.dumps(self.json_config)))
+        self.check_result("Process", results)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        image = self.visualize_result(image)
+        image = cv2.resize(image, display_size)
+        if self.is_result_show:
+            cv2.imshow("License Plate Recognition", image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-        images = glob.glob(os.path.join(self.image_folder, '*.*'))
-        for image_path in images:
-            if not os.path.isfile(image_path):
-                raise OSError("File doesn't exist: %s" % image_path)
-
-            image, image_type = self.load_cv2_image(image_path)
-            height, width, channels = image.shape
-            results = ultimateAlprSdk.UltAlprSdkEngine_process(
-                image_type,
-                image.tobytes(),
-                width,
-                height,
-                0,
-                1
-            )
-
-            self.check_result("Process", results)
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            image = self.visualize_result(image)
-            image = cv2.resize(image, display_size)
-            if self.is_result_show:
-                cv2.imshow("License Plate Recognition", image)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-
-            # agar save true bolsa rasmni berilgan papkaga saqlash
-            if self.is_save:
-                self.save_result_image(image, image_path)
-        self.check_result("DeInit", ultimateAlprSdk.UltAlprSdkEngine_deInit())
+        # agar save true bolsa rasmni berilgan papkaga saqlash
+        if self.is_save:
+            self.save_result_image(image, image_path)
 
 
 if __name__ == "__main__":
@@ -251,7 +240,10 @@ if __name__ == "__main__":
 
         "recogn_rectify_enabled": True,
         "recogn_minscore": 0.3,
-        "recogn_score_type": "min"
+        "recogn_score_type": "min",
+
+        "assets_folder": os.path.join(PROJECT_DIR, 'assets'),
+        "charset": "latin"
     }
 
     IMAGE_TYPES_MAPPING = {
@@ -259,6 +251,24 @@ if __name__ == "__main__":
         'RGBA': ultimateAlprSdk.ULTALPR_SDK_IMAGE_TYPE_RGBA32,
         'L': ultimateAlprSdk.ULTALPR_SDK_IMAGE_TYPE_Y
     }
+    # rasmlar joyi
+    image_folder = os.path.join(PROJECT_DIR, 'assets', 'images')
+    # natija rasimlarni saqlash joyi
+    save_img_path = os.path.join(PROJECT_DIR, 'assets', 'result_imgs')
 
-    recognizer = LicensePlateRecognizer(JSON_CONFIG, IMAGE_TYPES_MAPPING)
-    recognizer.process_images()
+    recognizer = LicensePlateRecognizer(IMAGE_TYPES_MAPPING,
+                                        is_save=True,
+                                        save_path=save_img_path,
+                                        is_result_show=False)
+
+    recognizer.check_result("Init",
+                            ultimateAlprSdk.UltAlprSdkEngine_init(
+                                json.dumps(JSON_CONFIG)))
+
+    images = glob.glob(os.path.join(image_folder, '*.*'))
+    for image_path in images:
+        if not os.path.isfile(image_path):
+            raise OSError("File doesn't exist: %s" % image_path)
+        recognizer.process_images(image_path)
+
+    recognizer.check_result("DeInit", ultimateAlprSdk.UltAlprSdkEngine_deInit())
